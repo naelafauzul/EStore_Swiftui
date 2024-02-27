803427e0e792bb24821c6dc85acfed233d346fb4
naelafauzul <naelafauzulm@gmail.com>
2024-02-27 20:14:04 +0700

    Initial Commit

---
+++ EStore_Swiftui/EStore_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EStore_SwiftuiApp.swift
+//  EStore_Swiftui
+//
+//  Created by Naela Fauzul Muna on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EStore_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
